40d51446efe3d3b396ff86604d6ee4869902db94
Banzoli <guilherme.banzoli@gmail.com>
2022-05-02 14:07:39 -0300

    cadastro base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
