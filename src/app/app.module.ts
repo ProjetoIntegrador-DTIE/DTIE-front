7f13ce10d6977a72e4d5f6b8ac4d23c773b11c44
BruunoSobral <brunocole19@gmail.com>
2022-05-02 15:06:32 -0300

    Sobre Nós

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
