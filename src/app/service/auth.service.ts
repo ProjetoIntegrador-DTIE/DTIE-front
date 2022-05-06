f64daadd17c19edc6fa73ca2ce3f95fdf979c18a
lucas bentes <lucasbentes16@gmail.com>
2022-05-06 14:53:37 -0300

    Mudanca na apresentacao do app.component

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../modal/Usuario';
 import { UsuarioLogin } from '../modal/UsuarioLogin';
 
@@ -17,6 +18,14 @@ export class AuthService {
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
     return this.http.post<Usuario>("https://dtie.herokuapp.com/usuarios/cadastrar", usuario);
+  }
+
+  logado(){
+    let ok: boolean = false;
+    if(environment.token != ""){
+      ok = true;
+    }
+    return ok;
   }
 
 }
