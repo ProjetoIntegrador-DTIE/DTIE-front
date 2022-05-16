de902f0e0448d0301bbe260930900ae783cdf04b
lucas bentes <lucasbentes16@gmail.com>
2022-05-16 14:39:50 -0300

    order by

--- src/app/postagem/postagem.component.ts
+++ src/app/postagem/postagem.component.ts
@@ -33,6 +33,9 @@ export class PostagemComponent implements OnInit {
 
   cep: Cep = new Cep()
 
+  key = "date"
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
