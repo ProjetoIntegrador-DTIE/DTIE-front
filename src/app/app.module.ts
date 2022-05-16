23791f8934c00a1ddd86fd94284240cb1a8480ee
Banzoli <guilherme.banzoli@gmail.com>
2022-05-16 11:23:02 -0300

    adm att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.compon
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { OrderModule } from 'ngx-order-pipe';
 
 @NgModule({
   declarations: [
@@ -29,15 +30,15 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     TemaComponent,
     PostagemEditComponent,
     TemaEditComponent,
-    TemaDeleteComponent,
-    PostagemDeleteComponent
+    TemaDeleteComponent
   ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    OrderModule
   ],
 
   providers: [
